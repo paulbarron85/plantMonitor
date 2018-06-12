068a7d7c91afaa41d39244713c6713115c9d67f8
PiesBarron <paulnbarron@gmail.com>
2018-06-12 16:17:50 +0200

    Initial commit
    Toggle a pin off and on every second.

---
+++ plant.py
@@ -0,0 +1,21 @@
+import RPi.GPIO as GPIO
+import time
+
+print "Hello World!"
+
+# Pin Definitions:
+pumpPin = 23 # Broadcom pin 23 (P1 pin 16)
+
+# Pin setup
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pumpPin, GPIO.OUT)
+
+try:
+    while 1:
+        GPIO.output(pumpPin, GPIO.HIGH)
+        time.sleep(1)
+        GPIO.output(pumpPin, GPIO.LOW)
+        time.sleep(1)
+except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
+    print "Goodbye World!"
+    GPIO.cleanup()
